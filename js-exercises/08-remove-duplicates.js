4e0153da9163a26a25fc5e37ad98d9fd29c3f5b3
rossana87 <rossana.ventrella@gmail.com>
2026-05-04 17:43:32 +0100

    chore(): remove duplicates task

---
+++ js-exercises/08-remove-duplicates.js
@@ -0,0 +1,19 @@
+function uniqueSet(arr){
+    return [...new Set(arr)]
+}
+
+function unique(arr){
+    let result = [];
+
+    for(let item of arr){
+        if(!result.includes(item)){
+            result.push(item)
+        }
+    }
+    return result
+}
+
+
+console.log(uniqueSet([1, 2, 2, 3, 3, 3, 4]))
+console.log(unique([1, 2, 2, 3, 3, 3, 4]))
+
