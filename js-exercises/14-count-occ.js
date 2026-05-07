c5d76bc059834b693279b6dec6beffdca630cb19
rossana87 <rossana.ventrella@gmail.com>
2026-05-07 11:53:06 +0100

    chore: count occ task

---
+++ js-exercises/14-count-occ.js
@@ -0,0 +1,25 @@
+function countOccurrences(arr, value){
+    let count = 0
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === value){
+            count++
+        }
+    }
+    return count
+}
+
+function countOcc(arr, value){
+    return arr.filter((num) => num === value).length;
+}
+
+function countOccReduce(arr, value){
+    return arr.reduce((acc, initialVal) => {
+
+        if(initialVal === value){
+           return acc + 1
+        }
+        return acc
+       },0)
+    }
+
+console.log(countOccReduce([1, 2, 2, 3, 2, 4], 2))
