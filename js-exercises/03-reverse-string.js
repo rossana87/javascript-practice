b340bb9213915815d9edc5d12acb33de0b6a73ac
rossana87 <rossana.ventrella@gmail.com>
2026-05-07 12:54:58 +0100

    chore: longest word task

--- js-exercises/03-reverse-string.js
+++ js-exercises/03-reverse-string.js
@@ -3,3 +3,24 @@ function reverse(str) {
 }
 
 //console.log(reverse("hello"))
+
+function reverseForLoop(str){
+    let result = "";
+    for (let i = 0; i < str.length; i++ ){
+        
+        result = str[i] + result
+    }
+    return result
+}
+
+//console.log(reverseForLoop("hello"))
+
+function reverseBackwordForLoop(str) {
+    let result = "";
+    for (let i = str.length - 1; i >= 0; i--){
+       result += str[i]
+    }
+    return result
+}
+
+//console.log(reverseBackwordForLoop("hello"))
---
+++ js-exercises/18-longest-word.js
@@ -0,0 +1,13 @@
+function longestWord(sentence){
+    let newSentence = sentence.split(' ');
+
+    let word = newSentence[0];
+    for(let i = 0; i < newSentence.length; i++){
+        if(newSentence[i].length > word.length){
+            word = newSentence[i]
+        }
+    }
+    return word
+}
+
+console.log(longestWord("I am learning JavaScript"))
